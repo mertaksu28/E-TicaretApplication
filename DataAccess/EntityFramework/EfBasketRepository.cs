2b266c8fc967cf64b352e8ba48d6b09507e2e695
mertaksu28 <mertaksu28@outlook.com.tr>
2021-09-30 19:41:19 +0300

    webapi

--- DataAccess/EntityFramework/EfBasketRepository.cs
+++ DataAccess/EntityFramework/EfBasketRepository.cs
@@ -1,11 +1,7 @@
 ﻿using DataAccess.Abstract;
 using DataAccess.Repository;
 using Entity.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DataAccess.EntityFramework
 {
